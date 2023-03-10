4522637ea49cd084df9917bd4d8f30b70498de61
Stephen <stephenobi441@gmail.com>
2023-03-11 00:01:52 +0100

    code fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -22,14 +22,10 @@ int _atoi(char *s)
 	while (s[len] != '\0')
 
 		len++;
-
-
 	while (i < len && f == 0)
 	{
-
 		if (s[i] == '-')
 			++d;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -38,50 +34,37 @@ int _atoi(char *s)
 				digit = -digit;
 
 			n = n * 10 + digit;
-
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-
 				break;
 			f = 0;
 		}
-
 		i++;
 	}
 
 	if (f == 0)
-
 		return (0);
-
 	return (n);
 }
-
 /**
  * main - multiplies two numbers
- * @argc: number of argument
+ * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 (Success), 1 (Error
+ * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
 {
-
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 	{
-
 		printf("Error\n");
 		return (1);
 	}
-
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-
 	printf("%d\n", result);
-
 	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -31,18 +31,15 @@ int check_num(char *str)
 	}
 
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+*/
 
 	int main(int argc, char *argv[])
-
-
 	{
 
 
